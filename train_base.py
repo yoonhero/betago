b3c69fa5f6e4340f9cdc5b88c9d6bf780fd21764
yoonhero <yoonhero06@naver.com>
2023-11-16 19:03:40 +0900

    fix: DataLoading

--- train_base.py
+++ train_base.py
@@ -4,11 +4,13 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 import einops
+import multiprocessing
 
 from pathlib import Path
 import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 from base import Unet
 
@@ -16,51 +18,57 @@ class GOMUDataset(Dataset):
     def __init__(self, max_idx, device):
         super().__init__()
         self.device = device
+        self.base_path = Path("./dataset/processed")
         self.x, self.y = self._load(max_idx)
+        
         self.total = self.x.shape[0]
-    
+        # self.total = max_idx
+
     def _load(self, max_idx):
-        inputs = np.array([])
-        outputs = np.array([])
+        inputs = []
+        outputs = []
 
-        base_path = Path("./dataset/processed")
-        for i in range(max_idx+1):
-            file_path = base_path / f"{i:05d}.npz"
-            data = np.load(file_path)
-
-            _inputs = data["inputs"]
-            _outputs = data["outputs"]
-
-            if inputs.shape == (0,):
-                inputs = _inputs.copy()
-                outputs = _outputs.copy()
-                continue
-
-            inputs = np.concatenate([inputs, _inputs])
-            outputs = np.concatenate([outputs, _outputs])
-            
-        inputs = torch.from_numpy(inputs)
-        outputs = torch.from_numpy(outputs)
-
+        for i in tqdm.tqdm(range(max_idx+1)):
+            # inputs, outputs = p.map(self._load_file, list(range(max_idx+1)))
+            _inputs, _outputs = self._load_file(i)
+            inputs.append(_inputs)
+            outputs.append(_outputs)
+                
+        inputs = np.concatenate(inputs)
+        outputs = np.concatenate(outputs)
+        
         return inputs, outputs
+   
+    def _load_file(self, i):
+        file_path = self.base_path / f"{i:05d}.npz"
+        data = np.load(file_path)
+        
+        _inputs = data["inputs"]
+        _outputs = data["outputs"]
+        
+        return _inputs, _outputs
 
     def __getitem__(self, idx):
-        turns = torch.sum(self.x[idx]!=0)
+        # x, y = self._load_file(idx)
+        x, y = self.x[idx], self.y[idx]
+        x, y = torch.from_numpy(x), torch.from_numpy(y)
+
+        turns = torch.sum(x!=0)
         is_white = not bool(turns % 2)
-        x = self.x[idx] if is_white else -self.x[idx]
-        y = self.y[idx]
+        x *= 1 if is_white else -1
 
         x = einops.rearrange(x, "h w -> 1 h w")
         y = einops.rearrange(y, "h w -> 1 h w")
         y = y.argmax()
+        x = x.to(torch.float32)
 
-        return x, y
+        return x.to(self.device), y.to(self.device)
 
     def __len__(self):
         return self.total
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-full_dataset = GOMUDataset(14087, device=device)
+full_dataset = GOMUDataset(8000, device=device)
 train_size = int(0.8 * len(full_dataset))
 test_size = len(full_dataset) - train_size
 train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
@@ -74,6 +82,8 @@ channels = [1, 4, 16]
 net = Unet(nrow=nrow, ncol=ncol, channels=channels).to(device)
 learning_rate = 0.001
 optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+
+save_base_path = Path(f"./tmp/history_{int(time.time()*1000)}")
 
 def one_loop(loader, net, optimizer, is_training=True):
     _loss = []
@@ -108,6 +118,9 @@ for epoch in range(nb_epoch):
 
     train_losses.append(train_loss)
     test_losses.append(test_losses)
+
+    save_path = save_base_path / f"{epoch}.pkl"
+    torch.save({"model": net.state_dict(), "optim": optimizer.state_dict()}, )
 
     print(f"{epoch}th EPOCH DONE!! ---- Train: {train_loss} | Test: {test_loss}")
 
